6f7380fbf7ca66c3659c2687a1d1181426f2c0d1
IvanTrutnev <trutnev_1996@mail.ru>
2015-09-13 11:21:41 +0300

    homework 34.1 ready
    @isadchenko check please

--- dz34.1.cpp
+++ dz34.1.cpp
@@ -1,136 +1,122 @@
 #include <iostream>
-#include <windows.h>
-#include <conio.h>
-#include <cstring>
+#include <string>
+
 using namespace std;
-const int LEN = 80;
-const int MAX = 40;
-char buf[256];
-
-class Stack
-{
-private:
-	char st[MAX];
-	int top;
-public:
-	Stack()
-	{
-		top = 0;
-	}
-	void push(char var)
-	{
-		st[++top] = var;
-	}
-	char pop()
-	{
-		return st[top--];
-	}
-	int gettop()
-	{
-		return top;
-	}
-};
-
-class express
-{
-private:
-	Stack s;
-	char* pStr;
-	int Len;
-public:
-	express(char* ptr)
-	{
-		pStr = ptr;
-		Len = strlen(pStr);
-	}
-	void parse();
-	int solve();
-};
-
-
-void express::parse()
-{
-	char ch;
-	char lastval;
-	char lastop;
-
-	for (int j = 0; j < Len; j++)
-	{
-		ch = pStr[j];
-		if (ch >= '0' && ch <= '9')
-			s.push(ch - '0');
-		else
-			if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
-			{
-				if (s.gettop() == 1)
-					s.push(ch);
-				else
-				{
-					lastval = s.pop();
-					lastop = s.pop();
-					if ((ch == '*' || ch == '/') && (lastop == '+' || lastop == '-'))
-					{
-						s.push(lastop);
-						s.push(lastval);
-					}
-					else
-					{
-						switch (lastop)
-						{
-						case '+': s.push(s.pop() + lastval); break;
-						case '-': s.push(s.pop() - lastval); break;
-						case '*': s.push(s.pop() * lastval); break;
-						case '/': s.push(s.pop() / lastval); break;
-						}
-					}
-					s.push(ch);
-				}
-			}
-			else
-			{
-				exit(1);
-			}
-	}
-}
-
-int express::solve()
-{
-	char lastval;
-
-	while (s.gettop() > 1)
-	{
-		lastval = s.pop();
-		switch (s.pop())
-		{
-		case '+': s.push(s.pop() + lastval); break;
-		case '-': s.push(s.pop() - lastval); break;
-		case '*': s.push(s.pop() * lastval); break;
-		case '/': s.push(s.pop() / lastval); break;
-		default: exit(1);
-		}
-	}
-	return int(s.pop());
-}
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	char ans;
-	char string[LEN];
-	cout << ("\nВведите аримметическое выражение в виде 2+3*7/2"
-		"\nЧисла должны из одной цифры"
-		"\nНе используя пробелы и скрбки");
-	do
+	string inf;
+	string post;
+
+	cout << "Infix: \t";
+	getline(cin, inf);
+	inf.append(1u, '!');
+
+	char* stack_1 = new char[inf.length()];
+	int idx_top = -1;
+
+	int i = 0;
+	while (inf[i] != '\0'){
+		switch (inf[i])
+		{
+		case '!':
+			if (idx_top == -1)
+			{
+				cout << "OK!\n";
+				++i;
+			}
+			else if (stack_1[idx_top] == '+' || stack_1[idx_top] == '-' || stack_1[idx_top] == '*' || stack_1[idx_top] == '/'){
+				post.append(1u, stack_1[idx_top--]);
+			}
+			else if (stack_1[idx_top] == '('){
+				cout << "Error!\n";
+				exit(0);
+			}
+			break;
+		case '+':
+		case '-':
+			if (idx_top == -1 || stack_1[idx_top] == '(')
+			{
+				stack_1[++idx_top] = inf[i++];
+			}
+			else if (stack_1[idx_top] == '+' || stack_1[idx_top] == '-' || stack_1[idx_top] == '*' || stack_1[idx_top] == '/'){
+				post.append(1u, stack_1[idx_top--]);
+			}
+			break;
+
+		case '*':
+		case '/':
+			if (stack_1[idx_top] == '+' || stack_1[idx_top] == '-' || idx_top == -1 || stack_1[idx_top] == '(')
+			{
+				stack_1[++idx_top] = inf[i++];
+			}
+			else if (stack_1[idx_top] == '*' || stack_1[idx_top] == '/'){
+				post.append(1u, stack_1[idx_top--]);
+			}
+			break;
+		case '(':
+			stack_1[++idx_top] = inf[i++];
+			break;
+		case ')':
+			if (idx_top == -1)
+			{
+				cout << "wtf!\n";
+				exit(0);
+			}
+			else if (stack_1[idx_top] == '+' || stack_1[idx_top] == '-' || stack_1[idx_top] == '*' || stack_1[idx_top] == '/'){
+				post.append(1u, stack_1[idx_top--]);
+			}
+			else if (stack_1[idx_top--] == '('){
+				++i;
+			}
+			break;
+		default:
+			post.append(1u, inf[i++]);
+			break;
+		}
+
+	}
+
+	inf.erase(inf.find('!'));
+	cout << "postfix: " << post << endl;
+	delete[] stack_1;
+
+	double* stack_2 = new double[post.length()];
+	idx_top = -1;
+	int j = 0;
+
+	while (post[j] != '\0')
 	{
-		cout << ("\nВыражение : ");
-		cin >> string;
-		express* eptr = new express(string);
-		eptr->parse();
-		cout << ("\nРезультат : ") << eptr->solve();
-		delete eptr;
-		cout << ("\nЕще одно выражение (d/n)? ");
-		cin >> ans;
-	} while (ans == 'd');
-	getch();
+		if ((int)post[j] > 47 && (int)post[j] < 58)
+		{
+			stack_2[++idx_top] = (double)post[j] - 48;
+		}
+		else{
+			double second = stack_2[idx_top];
+			double first = stack_2[idx_top - 1];
+			switch (post[j])
+			{
+			case '+':
+				stack_2[--idx_top] = first + second;
+				break;
+			case '-':
+				stack_2[--idx_top] = first - second;
+				break;
+			case '*':
+				stack_2[--idx_top] = first * second;
+				break;
+			case '/':
+				stack_2[--idx_top] = first / second;
+				break;
+			default:
+				break;
+			}
+		}
+		++j;
+	}
+
+	cout << endl << inf << " = " << stack_2[0] << endl;
+	delete[] stack_2;
+
 	return 0;
 }
